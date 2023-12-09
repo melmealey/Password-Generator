d60fe7e89ba1a68d53009d8039af85f630692634
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-09 17:20:29 -0500

    let length conditional working

--- script.js
+++ script.js
@@ -1,83 +1,40 @@
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
 
-let hasLength= ; //must be between 8-128 characters long
-let lower= ["a", "b", "c", "d","e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let upper= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let hasNumber= [1,2,3,4,5,6,7,8,9,0];
-let hasSpecialCharacter= ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",];
+let hasUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let hasLower = ["a", "b", "c", "d","e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let hasNumber = [1,2,3,4,5,6,7,8,9,0];
+let hasSpecialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",];
 
-const buttonClick =document.querySelector("button")
-const hasUpper = (event) => {
-    event.preventDefault()
-let hasUpper= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-//
-const buttonClick.addEventListener ("click", hasUpper)
+    
 
+let buttonClick =document.getElementById("generate")
+buttonClick.addEventListener("click", passwordGenerator);
+function passwordGenerator () {
+    let length =prompt("How many characters would you like your password to contain?");
+    if (length < 8){
+        alert ("Password must be at least 8 characters");
+        return null;
+    }
 
-// const hasLength =
-
+    if (length > 128){
+        alert ("Password must be less than 128 characters");
+        return null;
+    
+    }
 
 
 
 }
 
 
-console.log ("hasUpper")
 
+// const passwordGenerator = (event) => {
+//     event.preventDefault()
 
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-
-// let generateBtn = document.querySelector("#generate");
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// let hasLength= ; //must be between 8-128 characters long
-// let lower= ["a", "b", "c", "d","e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// let upper= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// let hasNumber= [1,2,3,4,5,6,7,8,9,0];
-// let hasSpecialCharacter= ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",];
-
-
-// // // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-
-
-
-// const generatePassword =() => {
-//     let password = generatePassword();
-//     let passwordText = document.querySelector("#password");
-
-// }
-
-// // //ok is true, cancel is false bollean
-
-
-
-
-// console.log ="click"
-
+//     let choice=prompt("Do you want to use Upper Case Letters? Press OK for Yes, and Cancel for No")
+//    const buttonClick =document.querySelector("button")
+    
+//     const hasLength >8, <128; 
+//     const upperCase "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     const lowerCase "abcdefghijklmnopqrstuvwxyz";
+//     const hasNumber "1234567890";
+//     const hasSpecialCharacter "!@#$%^&*()=+-?<>";
