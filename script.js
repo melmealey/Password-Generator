34ee267ea16bcdc914d725b639286adc52872c6f
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-08 21:36:50 -0500

    adding in password criteria characters

---
+++ script.js
@@ -0,0 +1,53 @@
+Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+let hasLength;
+let hasNumber;
+let upper;
+let lower;
+let hasSpecialCharacter;
+
+
+let hasLength= ;
+let lower= ["a", "b", "c", "d","e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upper= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let hasNumber= [1,2,3,4,5,6,7,8,9,0];
+let hasSpecialCharacter= ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",];
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+const generatePassword =() => {
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+
+}
+
+
+// // upper and lower =26 letters
+// // numbers is 10 (0-9)
+
+
+// //hasSpecialCharacters
+
+// //promt user for special
+// //if yes hasSpecial =yes
+
+// //ok is true, cancel is false bollean
+
+
+
+
+console.log 
+
