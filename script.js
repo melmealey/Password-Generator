72c280466e83f7b381b15842f39ba77b602a42ce
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-09 15:10:24 -0500

    adding function

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
-Assignment Code
-let generateBtn = document.querySelector("#generate");
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
 
-let hasLength;
-let hasNumber;
-let upper;
-let lower;
-let hasSpecialCharacter;
-
-
-let hasLength= ;
+let hasLength= ; //must be between 8-128 characters long
 let lower= ["a", "b", "c", "d","e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upper= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let hasNumber= [1,2,3,4,5,6,7,8,9,0];
 let hasSpecialCharacter= ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",];
 
+const buttonClick =document.querySelector("button")
+const hasUpper = (event) => {
+    event.preventDefault()
+let hasUpper= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//
+const buttonClick.addEventListener ("click", hasUpper)
 
-// // Write password to the #password input
+
+// const hasLength =
+
+
+
+
+}
+
+
+console.log ("hasUpper")
+
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+
+// let generateBtn = document.querySelector("#generate");
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// let hasLength= ; //must be between 8-128 characters long
+// let lower= ["a", "b", "c", "d","e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// let upper= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// let hasNumber= [1,2,3,4,5,6,7,8,9,0];
+// let hasSpecialCharacter= ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",];
+
+
+// // // Write password to the #password input
 // function writePassword() {
 //   var password = generatePassword();
 //   var passwordText = document.querySelector("#password");
@@ -24,30 +65,19 @@ let hasSpecialCharacter= ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",",
 
 // }
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
 
 
-const generatePassword =() => {
-    let password = generatePassword();
-    let passwordText = document.querySelector("#password");
 
-}
+// const generatePassword =() => {
+//     let password = generatePassword();
+//     let passwordText = document.querySelector("#password");
 
+// }
 
-// // upper and lower =26 letters
-// // numbers is 10 (0-9)
-
-
-// //hasSpecialCharacters
-
-// //promt user for special
-// //if yes hasSpecial =yes
-
-// //ok is true, cancel is false bollean
+// // //ok is true, cancel is false bollean
 
 
 
 
-console.log 
+// console.log ="click"
 
