f85f248ef7be6c0ffba06e91de0ddceefeed3aed
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-10 14:50:33 -0500

    name fixes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 //password criteria
-let hasUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let hasLower = ["a", "b", "c", "d","e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let hasNumber = [1,2,3,4,5,6,7,8,9,0];
-let hasSpecialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",];
+let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lower = ["a", "b", "c", "d","e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let number = [1,2,3,4,5,6,7,8,9,0];
+let specialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",];
 
 let generateBtn =document.getElementById("generate")
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-function writePassword () {
+function writePassword () { 
 
     let length =prompt("How many characters would you like your password to contain?");
 
@@ -22,6 +22,7 @@ function writePassword () {
         return null;
     
     }
+
     let hasUpper =confirm("Would you like to include Upper Case characters? Press Ok for YES or Cancel for NO");
     let hasLower =confirm("Would you like to include Lower Case characters? Press Ok for YES or Cancel for NO");
     let hasNumber =confirm("Would you like to include numbers? Press OK for YES of Cancel for NO")
@@ -29,20 +30,6 @@ function writePassword () {
     
     if (! (hasUpper || hasLower|| hasNumber || hasSpecialCharacter)) {
         alert ("Must select at least 1 character type");
-        return passwordGenerator();
-
+        return null;
     }
-     
-    window.alert("Your Random Password is ");
-
-    // document.passwordGenerator = () => {
-    //     alert(document.querySelector('input[type="password"]').value);
-
-    //   }
-
-       
-
 }
-
-
-//     event.preventDefault()
