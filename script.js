018a6c54ca519817e3d944c861a262a3d7bbdb3a
Mel Mel Mom <melmelmomof4@gmail.com>
2023-12-11 22:31:05 -0500

    added Math Random, for loops and if variables

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
+//variables
 let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let lower = ["a", "b", "c", "d","e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let number = [1,2,3,4,5,6,7,8,9,0];
 let specialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@",];
 
+//Passwrod Criteria
 function generatePassword () { 
 
     let length =prompt("How many characters would you like your password to contain?");
@@ -21,33 +23,48 @@ function generatePassword () {
     
     }
 
+//User gets to choose
     let hasUpper =confirm("Would you like to include Upper Case characters? Press Ok for YES or Cancel for NO");
     let hasLower =confirm("Would you like to include Lower Case characters? Press Ok for YES or Cancel for NO");
     let hasNumber =confirm("Would you like to include numbers? Press OK for YES of Cancel for NO")
     let hasSpecialCharacter =confirm("Would you like to include Special characters? Press Ok for YES or Cancel for NO");
     
-    if (! (hasUpper || hasLower|| hasNumber || hasSpecialCharacter)) {
+    if (!hasUpper && !hasLower && !hasNumber && !hasSpecialCharacter) {
         alert ("Must select at least 1 character type");
         return null;
     }
 
+//Taking all the users choices and creating that random password
+let choices= ""; 
+    if (upper){
+        choices+= "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+    }
+    if (lower){
+        choices+= "abcdefghijklmnopqrstuvwxyz"
+    }
+    if (number){
+        choices+= "12345667890"
+    }
+    if (specialCharacter){
+        choices+= "!#$%&'()*+,-./:;<=>?@" 
+    }
+
+let password = "";
+    for(let i = 0; i <length; i++) {
+    password += Math.floor(Math.random() * choices.length); 
+
 }
+
  // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
-//if criteria has been met
-if (length >8 && <128 && (hasUpper || hasLower|| hasNumber || hasSpecialCharacter)) {
-    alert "password";
+
 }
 
 }
+console.log (password);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
-
-// 1. You should have a variable declared that you will be attaching your password characters to.
-// 2. You should have an if condition to determine if the password length has reached the desired length. if so then break from the for loop
-// 3. YOu should have a for loop that handles looping until the length of the password is met
-// 4. in your loop you will be selecting cahracters from your arrays at random indexes and attaching those chaacters to the password string
